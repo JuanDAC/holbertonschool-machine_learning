6a12e799be982239cf5335bf58ef5d2a51ce84ed
JuanDAC <juanavila12.0308@gmail.com>
2022-04-21 22:34:20 -0500

    [🚀] (add final version of integrals)

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -6,6 +6,8 @@ def poly_integral(poly, C=0, power=1):
     """ This function is used to generate poly integral function """
     if type(poly) is not list or type(C) not in (int, float):
         return None
+    if len(poly) == 0 and power == 1:
+        return None
 
     coefficient, *poly = [*poly, None]
 
@@ -14,8 +16,11 @@ def poly_integral(poly, C=0, power=1):
     if coefficient is None:
         return []
 
+    if coefficient == 0 and len(integrals) == 1:
+        return [*integrals, *poly_integral(poly, C, power + 1)]
+
     if coefficient == 0:
-        return [*integrals, 0, *poly_integral(poly, C, power + 1)]
+        return [0, *poly_integral(poly, C, power + 1)]
 
     if power != 0:
         result = (coefficient / power)
@@ -26,7 +31,14 @@ def poly_integral(poly, C=0, power=1):
 
 
 if __name__ == "__main__":
+    """
+    """
     poly = [5, 3, 0, 1]
     print(poly_integral(poly))
     print(poly_integral([7, 4, 6, 1, 5]))
     print(poly_integral([4, 8, 2, 4, 7, 1, 9], C=5))
+    print(poly_integral([0]))
+    print(poly_integral([0], C=7))
+    print(poly_integral(5))
+    print(poly_integral([]))
+    print(poly_integral([5], C=None))
