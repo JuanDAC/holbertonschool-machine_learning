edce6ff3fd433af8047747bfc544ba48c89f0063
JuanDAC <juanavila12.0308@gmail.com>
2022-05-12 23:55:05 -0500

    [🚨] (importing other way)

---
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Accuracy of neural network"""
+
+
+import tensorflow.compat.v1 as tf
+
+
+def train(X_train, Y_train, X_valid,
+          Y_valid, layer_sizes, activations, alpha,
+          iterations, save_path="/tmp/model.ckpt"):
+    """" Function to train a model with tensorflow"""
+    pass
---
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+
+def one_hot(Y, classes):
+    """convert an array to a one-hot matrix"""
+    pass
