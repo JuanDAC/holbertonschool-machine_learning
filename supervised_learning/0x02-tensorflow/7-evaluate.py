7a8d2052c4d0686417fd246eaea89f630d337bcc
JuanDAC <juanavila12.0308@gmail.com>
2022-06-06 08:20:06 -0500

    [🔥] (add tensor flow)

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -1,6 +1,35 @@
 #!/usr/bin/env python3
+"""Evaluation method for classification model"""
 
 
-def one_hot(Y, classes):
-    """convert an array to a one-hot matrix"""
-    pass
+import tensorflow.compat.v1 as tf
+
+
+def evaluate(X, Y, save_path):
+    """
+    Evaluation method
+    for classification model
+    ...
+    Parameters
+    __________
+    X : Tensor
+        The loss of the network’s prediction
+    Y : Tensor
+        The learning rate
+    ...
+    Return
+    ______
+    accuracy : Tensor
+        Operation that trains the network using gradient descent
+    """
+    with tf.Session() as sess:
+        saver = tf.train.import_meta_graph(save_path + '.meta')
+        saver.restore(sess, save_path)
+        x = tf.get_collection('x')[0]
+        y = tf.get_collection("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        loss = tf.get_collection("loss")[0]
+        acc = tf.get_collection("accuracy")[0]
+        vars = sess.run([y_pred, acc, loss], feed_dict={x: X, y: Y})
+        return vars[0], vars[1], vars[2]
+
