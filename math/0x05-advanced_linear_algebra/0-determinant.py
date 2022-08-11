e0d44c60a84ba1a285223166e071c16473c6c2a0
JuanDAC <juanavila12.0308@gmail.com>
2022-08-11 09:23:42 -0500

    [🐛] (add readme)

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -2,34 +2,46 @@
 
 
 def mirror(matrix, x):
-  return [matrix[i][1:] for i in range(len(matrix)) if i != x]
-      
+    """ mirror(matrix, x) returns a matrix with the xth row removed """
+    return [matrix[i][1:] for i in range(len(matrix)) if i != x]
 
-def shape(matrix = []):
-  if not matrix or type(matrix) != list:
-    return (0, 0, 0)
 
-  return (len(matrix), *shape(matrix[0]))
+def shape(matrix=[]):
+    """ shape(matrix) returns the shape of a matrix """
+    if not matrix or type(matrix) != list:
+        return (0, 0, 0)
+
+    return (len(matrix), *shape(matrix[0]))
+
+
+def get_sign(x):
+    """ get_sign(i, j) returns the sign of the determinant """
+    return (-1) ** (x)
 
 
 def determinant(matrix):
-  if type(matrix) != list or type(matrix[0]) != list:
-    raise TypeError("matrix must be a list of lists")
+    """ determinant(matrix) returns the determinant of a matrix """
+    if type(matrix) != list or type(matrix[0]) != list:
+        raise TypeError("matrix must be a list of lists")
 
-  first_dimention, second_dimention, *_ = shape(matrix)
+    first_dimention, second_dimention, *_ = shape(matrix)
 
-  if (first_dimention == 1 and second_dimention == 1):
-    return matrix[0][0]
+    if (first_dimention == 1 and second_dimention == 1):
+        return matrix[0][0]
 
-  if (first_dimention == 1 and second_dimention == 0):
-    return 1
+    if (first_dimention == 1 and second_dimention == 0):
+        return 1
 
-  if first_dimention != second_dimention:
-    raise ValueError("matrix must be a square matrix")
+    if first_dimention != second_dimention:
+        raise ValueError("matrix must be a square matrix")
 
-  if first_dimention == 2:
-    return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    if first_dimention == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
 
-  vector = list(zip(*matrix))[0]
+    vector = list(zip(*matrix))[0]
 
-  return sum(vector[i] * (-1) ** i * determinant(mirror(matrix, i)) for i in range(len(vector)))
+    return sum(
+        vector[i]
+        * get_sign(i)
+        * determinant(mirror(matrix, i)) for i in range(first_dimention)
+    )
