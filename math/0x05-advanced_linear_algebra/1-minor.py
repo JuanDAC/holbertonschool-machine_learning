90d0908f648901d0f44ca49144b02928ca34a6b7
JuanDAC <juanavila12.0308@gmail.com>
2022-08-21 11:45:51 -0500

    [🚀] (add minor)

--- math/0x05-advanced_linear_algebra/1-minor.py
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -2,7 +2,6 @@
 """ minor of matrix """
 
 
-
 def reduce(callback, list_of_lists):
     """ reduce(callback, list_of_lists) returns the result of the
     callback function applied to the list of lists """
@@ -29,43 +28,51 @@ def get_sign(x):
     """ get_sign(i, j) returns the sign of the determinant """
     return (-1) ** (x)
 
+
 def shape(matrix=[]):
-  if not matrix or type(matrix) != list:
-    return (0, 0, 0)
-  return (len(matrix), *shape(matrix[0]))
+    """ shape of matrix """
+    if not matrix or type(matrix) != list:
+        return (0, 0, 0)
+    return (len(matrix), *shape(matrix[0]))
+
 
 def mirror(matrix, x, y):
     """ mirror(matrix, x) returns a matrix with the xth row removed """
     return [
-      [*matrix[i][:y], *matrix[i][y + 1:]]
-      for i in range(len(matrix)) if i != x
+        [*matrix[i][:y], *matrix[i][y + 1:]]
+        for i in range(len(matrix))
+        if i != x
     ]
 
 
 def determinant_minor(matrix, x, y):
-  mirror_of_matrix = mirror(matrix, x, y) 
-  return mirror_of_matrix[0][0] * mirror_of_matrix[1][1] - mirror_of_matrix[0][1] * mirror_of_matrix[1][0]
+    """ determinat minor """
+    mirror_of_matrix = mirror(matrix, x, y)
+    return mirror_of_matrix[0][0] * mirror_of_matrix[1][1] \
+        - mirror_of_matrix[0][1] * mirror_of_matrix[1][0]
 
 
 def minor(matrix):
-  if validation_of_types(matrix):
-    raise TypeError("matrix must be a list of lists")
+    """ minor of a matrix """
 
-  first_dimention, second_dimention, *_ = shape(matrix)
+    if validation_of_types(matrix):
+        raise TypeError("matrix must be a list of lists")
 
-  if first_dimention != second_dimention:
-    raise ValueError("matrix must be a non-empty square matrix")
+    first_dimention, second_dimention, *_ = shape(matrix)
 
-  if first_dimention == 1 and second_dimention == 1:
-    return [[1]]
+    if first_dimention != second_dimention:
+        raise ValueError("matrix must be a non-empty square matrix")
 
-  if first_dimention == 2 and second_dimention == 2:
-    return [matrix[1][1], matrix[1][0]], [matrix[0][1], matrix[0][0]]
+    if first_dimention == 1 and second_dimention == 1:
+        return [[1]]
 
-  return transpose([
-    [
-      determinant_minor(matrix, x, y) * get_sign(x + y)
-      for x in range(second_dimention)
-    ]
-    for y in range(first_dimention)
-  ])
+    if first_dimention == 2 and second_dimention == 2:
+        return [matrix[1][1], matrix[1][0]], [matrix[0][1], matrix[0][0]]
+
+    return transpose([
+        [
+            determinant_minor(matrix, x, y) * get_sign(x + y)
+            for x in range(second_dimention)
+        ]
+        for y in range(first_dimention)
+    ])
