ffcb81d338815ae0743dd496321ec81cc08e7119
JuanDAC <juanavila12.0308@gmail.com>
2022-08-21 22:52:44 -0500

    [🚀] (add cofactor)

--- math/0x05-advanced_linear_algebra/2-cofactor.py
+++ math/0x05-advanced_linear_algebra/2-cofactor.py
@@ -81,7 +81,7 @@ def cofactor(matrix):
     if first_dimention == 3 and second_dimention == 3:
         return transpose([
             [
-                determinant_minor(matrix, x, y) * get_sign(x + y) 
+                determinant_minor(matrix, x, y) * get_sign(x + y)
                 for x in range(second_dimention)
             ]
             for y in range(first_dimention)
