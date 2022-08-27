7e26f1c2d08b6573118fb7a35297b2ae64bf9757
JuanDAC <juanavila12.0308@gmail.com>
2022-08-27 12:24:39 -0500

    [🐛] (fix cofactor )

--- math/0x05-advanced_linear_algebra/4-inverse.py
+++ math/0x05-advanced_linear_algebra/4-inverse.py
@@ -46,6 +46,13 @@ def mirror(matrix, x, y):
     ]
 
 
+def determinant_minor(matrix, x, y):
+    """ determinat minor """
+    mirror_of_matrix = mirror(matrix, x, y)
+    return mirror_of_matrix[0][0] * mirror_of_matrix[1][1] \
+        - mirror_of_matrix[0][1] * mirror_of_matrix[1][0]
+
+
 def all_shapes_equals(matrix):
     """ shapes of sub matrix """
     base_len = len(matrix)
@@ -53,45 +60,6 @@ def all_shapes_equals(matrix):
         (lambda acum, l: acum and base_len == len(l)),
         [True, *matrix]
     )
-
-
-def determinant(matrix):
-    """ determinant(matrix) returns the determinant of a matrix """
-
-    if validation_of_types(matrix):
-        raise TypeError("matrix must be a list of lists")
-
-    first_dimention, second_dimention, *_ = shape(matrix)
-
-    if (first_dimention == 1 and second_dimention == 1):
-        return matrix[0][0]
-
-    if (first_dimention == 1 and second_dimention == 0):
-        return 1
-
-    if first_dimention != second_dimention:
-        raise ValueError("matrix must be a square matrix")
-
-    if first_dimention == 2:
-        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-
-    vector = list(zip(*matrix))[0]
-
-    return sum(
-        vector[i]
-        * get_sign(i)
-        * determinant(mirror(matrix, i, 0)) for i in range(first_dimention)
-    )
-
-
-def determinant_minor(matrix, x, y):
-    """ determinat minor """
-    mirror_of_matrix = mirror(matrix, x, y)
-    if len(mirror_of_matrix) > 2:
-        return determinant(mirror_of_matrix) * get_sign(x + y)
-
-    return mirror_of_matrix[0][0] * mirror_of_matrix[1][1] \
-        - mirror_of_matrix[0][1] * mirror_of_matrix[1][0]
 
 
 def cofactor(matrix):
@@ -110,10 +78,11 @@ def cofactor(matrix):
     if first_dimention == 2 and second_dimention == 2:
         return [[matrix[1][1], -matrix[1][0]], [-matrix[0][1], matrix[0][0]]]
 
-    return transpose([
-        [
-            determinant_minor(matrix, x, y) * get_sign(x + y)
-            for x in range(second_dimention)
-        ]
-        for y in range(first_dimention)
-    ])
+    if first_dimention == 3 and second_dimention == 3:
+        return transpose([
+            [
+                determinant_minor(matrix, x, y) * get_sign(x + y)
+                for x in range(second_dimention)
+            ]
+            for y in range(first_dimention)
+        ])
