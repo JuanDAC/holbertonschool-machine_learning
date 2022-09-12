b3c95b0051400349f8fd1e0633669a080d0930d6
JuanDAC <juanavila12.0308@gmail.com>
2022-09-12 15:29:40 -0500

    (✏️) [correction from pycodestyle of correlation ]

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -6,17 +6,17 @@ import numpy as np
 def correlation(C):
     """
     Calculates the correlation of a matrix C where C is a numpy.ndarray
-    and its rows are variables and its columns are observations 
+    and its rows are variables and its columns are observations
 
     Validations:
     - If C is not a numpy.ndarray, raise a TypeError with the message
       "C must be a numpy.ndarray"
 
-    - If C does not have shape (d, d), raise a ValueError with the message 
+    - If C does not have shape (d, d), raise a ValueError with the message
       "C must be a 2D square matrix"
 
     Returns:
-    - A numpy.ndarray of shape (d, d) containing the correlation matrix 
+    - A numpy.ndarray of shape (d, d) containing the correlation matrix
       with the name correlation
 
     """
