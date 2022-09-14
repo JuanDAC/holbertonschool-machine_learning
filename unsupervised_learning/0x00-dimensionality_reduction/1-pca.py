be3fa90144a3640e14b693a9fad08beb97cda062
JuanDAC <juanavila12.0308@gmail.com>
2022-09-14 10:57:57 -0500

    (🐛) [fix lint of the performs]

--- unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -11,10 +11,10 @@ def pca(X, ndim):
     Function that performs PCA on a dataset
     Arguments:
     - X is a numpy.ndarray of shape (n, d) where:
-    - n is the number of data points 
+    - n is the number of data points
     - d is the number of dimensions in each point
     - ndim is the new dimensionality of the transformed X
-    Returns: 
+    Returns:
     - T, a numpy.ndarray of shape (n, ndim) containing
     the transformed version of X
     """
