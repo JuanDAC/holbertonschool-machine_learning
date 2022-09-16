f8b2f67cda7cdd9365b5a46c8cb69a0c54cc68e5
JuanDAC <juanavila12.0308@gmail.com>
2022-09-15 23:32:55 -0500

    [⚡️] (add agglomerative)

---
+++ unsupervised_learning/0x01-clustering/12-agglomerative.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+12 - Agglomerative
+"""
+
+import numpy as np
+from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
+import matplotlib.pyplot as plt
+
+
+def agglomerative(X, dist):
+    """
+    Arguments: 
+    - X: numpy.ndarray of shape (n, d) containing the dataset
+    - dist: the maximum cophenetic distance for all clusters
+    Returns:
+    - clss: numpy.ndarray of shape (n,) containing the cluster indices for each data point
+    """
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+        return None
+
+    if type(dist) != int or dist <= 0:
+        return None
+
+    Z = linkage(X, 'ward')
+    clss = fcluster(Z, t=dist, criterion='distance')
+    dendrogram(Z, color_threshold=dist)
+    plt.show()
+
+    return clss
