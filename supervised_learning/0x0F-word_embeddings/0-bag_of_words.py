ec6f5ab12a5890408c4abcb3af68b8bd84c618aa
JuanDAC <juanavila12.0308@gmail.com>
2022-09-18 23:18:35 -0500

    (🐛) [using sklearn]

--- supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
+++ supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
@@ -2,6 +2,7 @@
 """
 File that contains the bag_of_words function
 """
+from sklearn.feature_extraction.text import CountVectorizer
 
 
 def bag_of_words(sentences, vocab=None):
@@ -19,39 +20,8 @@ def bag_of_words(sentences, vocab=None):
          - f is the number of features analyzed
        - features is a list of the features used for embeddings
     """
-    if not isinstance(sentences, list) or len(sentences) == 0:
-        return None, None
-
-    if vocab is not None:
-        if not isinstance(vocab, list) or len(vocab) == 0:
-            return None, None
-
-    words = {}
-    for sentence in sentences:
-        if not isinstance(sentence, str):
-            return None, None
-
-        for word in sentence.split():
-            if word not in words:
-                words[word] = 1
-            else:
-                words[word] += 1
-
-    if vocab is not None:
-        for word in vocab:
-            if not isinstance(word, str):
-                return None, None
-
-            if word not in words:
-                words[word] = 1
-            else:
-                words[word] += 1
-
-    features = sorted(words.keys())
-    embeddings = np.zeros((len(sentences), len(features)))
-
-    for i, sentence in enumerate(sentences):
-        for j, feature in enumerate(features):
-            embeddings[i][j] = sentence.split().count(feature)
-
+    vectorizer = CountVectorizer(vocabulary=vocab)
+    X = vectorizer.fit_transform(sentences)
+    features = vectorizer.get_feature_names()
+    embeddings = X.toarray()
     return embeddings, features
