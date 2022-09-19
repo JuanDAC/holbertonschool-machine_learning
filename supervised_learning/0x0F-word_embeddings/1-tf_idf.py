3251296b8cd7a8973eaef9b5c3ff869279243bec
JuanDAC <juanavila12.0308@gmail.com>
2022-09-18 23:21:55 -0500

    [🚀] (add embedding function)

--- supervised_learning/0x0F-word_embeddings/1-tf_idf.py
+++ supervised_learning/0x0F-word_embeddings/1-tf_idf.py
@@ -1,1 +1,24 @@
 #!/usr/bin/env python3
+
+"""
+File that contains the tf_idf function
+
+"""
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def tf_idf(sentences, vocab=None):
+    """
+    Function that creates a bag of words embedding matrix
+    Arguments:
+      - sentences is a list of sentences to analyze
+      - vocab is a list of the vocabulary words to use for the analysis
+    Returns:
+      - embeddings, featuresj
+    """
+    vectorizer = TfidfVectorizer(vocabulary=vocab)
+    X = vectorizer.fit_transform(sentences)
+    features = vectorizer.get_feature_names()
+    embeddings = X.toarray()
+    return embeddings, features
