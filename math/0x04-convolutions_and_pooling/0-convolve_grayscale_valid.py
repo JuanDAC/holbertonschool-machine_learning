c7b1af4bd9b9d32a380d8a5419d4999b44fdd1d7
JuanDAC <juanavila12.0308@gmail.com>
2022-09-22 07:56:08 -0500

    [🚀] (fix convolve)

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -7,11 +7,13 @@ def convolve_grayscale_valid(images, kernel):
     """
     Function that performs a valid convolution on grayscale images
     Arguments:
-      - images is a numpy.ndarray with shape (m, h, w) containing multiple grayscale images
+      - images is a numpy.ndarray with shape (m, h, w) containing
+        multiple grayscale images
         * m is the number of images
         * h is the height in pixels of the images
         * w is the width in pixels of the images
-      - kernel is a numpy.ndarray with shape (kh, kw) containing the kernel for the convolution
+      - kernel is a numpy.ndarray with shape (kh, kw) containing
+        the kernel for the convolution
         * kh is the height of the kernel
         * kw is the width of the kernel
     Returns:
