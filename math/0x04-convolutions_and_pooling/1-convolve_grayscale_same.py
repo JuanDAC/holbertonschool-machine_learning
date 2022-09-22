b3bb3f7d235682e7dd446390af8dda25b52a17c7
JuanDAC <juanavila12.0308@gmail.com>
2022-09-22 14:09:53 -0500

    [🚀] (use keyworlds when create a padding)

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -9,11 +9,13 @@ def convolve_grayscale_same(images, kernel):
     """
     Function that performs a same convolution on grayscale images
     Arguments:
-      - images is a numpy.ndarray with shape (m, h, w) containing multiple grayscale images
+      - images is a numpy.ndarray with shape (m, h, w) containing
+        multiple grayscale images
         * m is the number of images
         * h is the height in pixels of the images
         * w is the width in pixels of the images
-      - kernel is a numpy.ndarray with shape (kh, kw) containing the kernel for the convolution
+      - kernel is a numpy.ndarray with shape (kh, kw) containing
+        the kernel for the convolution
         * kh is the height of the kernel
         * kw is the width of the kernel
     Returns:
@@ -23,10 +25,13 @@ def convolve_grayscale_same(images, kernel):
     kh, kw = kernel.shape
     ph = 0 if kh % 2 else int(kh / 2)
     pw = 0 if kw % 2 else int(kw / 2)
-    images_padding = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), 'constant')
+    images_padding = np.pad(images, pad_width=(
+        (0, 0), (ph, ph), (pw, pw)), mode='constant')
     convolved = np.zeros((m, h, w))
     for i in range(h):
         for j in range(w):
             convolved[np.arange(m), i, j] = np.sum(
-                images_padding[np.arange(m), i: i + kh, j: j + kw] * kernel, axis=(1, 2))
+                images_padding[np.arange(m), i: i + kh, j: j + kw]
+                * kernel, axis=(1, 2)
+            )
     return convolved
