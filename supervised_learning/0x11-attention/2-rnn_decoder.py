614db68b2e459477ac9e862de658ec2e6391306c
JuanDAC <juanavila12.0308@gmail.com>
2022-09-25 19:52:14 -0500

    [🚀] (fix shape of oputput layer)

--- supervised_learning/0x11-attention/2-rnn_decoder.py
+++ supervised_learning/0x11-attention/2-rnn_decoder.py
@@ -53,12 +53,17 @@ class RNNDecoder(tf.keras.layers.Layer):
           - hidden_states is a tensor of shape (batch, input_seq_len, units)
             * containing the outputs of the encoder
         Returns:
-          - y, s
+          - y is a tensor of shape (batch, vocab) containing the output word
+          as a one hot vector in the target vocabulary
+          - s is a tensor of shape (batch, units) containing the new decoder
+          hidden statek
         """
-        context = SelfAttention(s_prev)(hidden_states)
-        x = self.embedding(x)
-        x = tf.concat([tf.expand_dims(context, 1), x], axis=-1)
-        output, s = self.gru(x)
-        output = tf.reshape(output, (-1, output.shape[2]))
-        y = self.F(output)
+        _, units = s_prev.shape
+        context_vector, _ = SelfAttention(units)(s_prev, hidden_states)
+        embeddings = self.embedding(x)
+        input = tf.concat(
+            [tf.expand_dims(context_vector, 1), embeddings], axis=-1)
+        output, s = self.gru(input)
+        output_reshape = tf.reshape(output, (output.shape[0], output.shape[2]))
+        y = self.F(output_reshape)
         return y, s
