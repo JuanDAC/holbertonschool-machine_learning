6734ed729496dde57f226d4fae4f49eecf891198
JuanDAC <juanavila12.0308@gmail.com>
2022-09-25 22:59:21 -0500

    [🐛] (fix veriance generation un optimization)

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -22,39 +22,43 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     - d_vars: list containing the difference in variance from
               the smallest cluster size for each cluster size
     """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+    try:
+        if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+            return None, None
+
+        if type(kmin) != int or kmin <= 0:
+            return None, None
+
+        if kmax is None:
+            kmax = X.shape[0]
+
+        if type(kmax) != int or kmax <= 0:
+            return None, None
+
+        if type(iterations) != int or iterations <= 0:
+            return None, None
+
+        if not isinstance(kmin, int) or kmin < 1 or kmin >= X.shape[0]:
+            return None, None
+
+        kmeans = __import__('1-kmeans').kmeans
+        variance = __import__('2-variance').variance
+
+        results = []
+        d_vars = []
+
+        for k in range(kmin, kmax + 1):
+            C, clss = kmeans(X, k, iterations)
+            var = variance(X, C)
+            results.append((C, clss))
+            d_vars.append(var)
+
+        d_var = [0]
+        for i in range(kmax - 1):
+            derivate = d_vars[i] - d_vars[i + 1]
+            d_var.append(derivate + d_var[i])
+
+        return results, d_var
+
+    except Exception:
         return None, None
-
-    if type(kmin) != int or kmin <= 0:
-        return None, None
-
-    if kmax is None:
-        kmax = X.shape[0]
-
-    if type(kmax) != int or kmax <= 0:
-        return None, None
-
-    if type(iterations) != int or iterations <= 0:
-        return None, None
-
-    if not isinstance(kmin, int) or kmin < 1 or kmin >= X.shape[0]:
-        return None, None
-
-    kmeans = __import__('1-kmeans').kmeans
-    variance = __import__('2-variance').variance
-
-    results = []
-    d_vars = []
-
-    for k in range(kmin, kmax + 1):
-        C, clss = kmeans(X, k, iterations)
-        var = variance(X, C)
-        results.append((C, clss))
-        d_vars.append(var)
-
-    d_var = [0]
-    for i in range(kmax - 1):
-        derivate = d_vars[i] - d_vars[i + 1]
-        d_var.append(derivate + d_var[i])
-
-    return results, d_var
