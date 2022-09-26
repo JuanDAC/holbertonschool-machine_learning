b14c877b597d486e625759674d8fdde21595d111
JuanDAC <juanavila12.0308@gmail.com>
2022-09-26 18:03:28 -0500

    [🐛] (add forward)

--- unsupervised_learning/0x02-hmm/3-forward.py
+++ unsupervised_learning/0x02-hmm/3-forward.py
@@ -9,20 +9,28 @@ def forward(Observation, Emission, Transition, Initial):
     """
     Function that performs the forward algorithm for a hidden markov model
     Arguments:
-        - Observation is a numpy.ndarray of shape (T,) that contains the index of the observation
+        - Observation is a numpy.ndarray of shape (T,) that contains the index
+                      of the observation
             * T is the number of observations
-        - Emission is a numpy.ndarray of shape (N, M) containing the emission probability of a specific observation given a hidden state
-            * Emission[i, j] is the probability of observing j given the hidden state i
+        - Emission is a numpy.ndarray of shape (N, M) containing the emission
+                   probability of a specific observation given a hidden state
+            * Emission[i, j] is the probability of observing j given the hidden
+                             state i
             * N is the number of hidden states
             * M is the number of all possible observations
-        - Transition is a 2D numpy.ndarray of shape (N, N) containing the transition probabilities
-            * Transition[i, j] is the probability of transitioning from the hidden state i to j
-        - Initial a numpy.ndarray of shape (N, 1) containing the probability of starting in a particular hidden state
+        - Transition is a 2D numpy.ndarray of shape (N, N) containing the
+                     transition probabilities
+            * Transition[i, j] is the probability of transitioning from
+                               the hidden state i to j
+        - Initial a numpy.ndarray of shape (N, 1) containing the probability
+                  of starting in a particular hidden state
     Returns:
         - P, F, or None, None on failure
         - P is the likelihood of the observations given the model
-        - F is a numpy.ndarray of shape (N, T) containing the forward path probabilities
-        - F[i, j] is the probability of being in hidden state i at time j given the previous observations
+        - F is a numpy.ndarray of shape (N, T) containing the forward
+            path probabilities
+        - F[i, j] is the probability of being in hidden state i at
+                  time j given the previous observations
     """
     try:
         if type(Observation) is not np.ndarray or len(Observation.shape) != 1:
