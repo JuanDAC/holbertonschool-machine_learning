42eecace4fdf48dd3d9fb184a65791a32323e76c
JuanDAC <juanavila12.0308@gmail.com>
2022-09-25 22:34:17 -0500

    [🐛] (fix centroid cluster)

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -21,49 +21,19 @@ def kmeans(X, k, iterations=1000):
     """
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None
-
-    if type(k) != int or k <= 0:
+    if not isinstance(k, int) or k <= 0:
         return None, None
-
-    if type(iterations) != int or iterations <= 0:
+    if not isinstance(iterations, int) or iterations <= 0:
         return None, None
-
-    # Initialize the cluster centroids using a multivariate uniform
-    # distribution (based on0-initialize.py)
-    min = np.amin(X, axis=0)
-    max = np.amax(X, axis=0)
-
-    _, d = X.shape
-    C = np.random.uniform(min, max, size=(k, d))
-
-    # If a cluster contains no data points during the update step,
-    # reinitialize its centroid
-    # You should use numpy.random.uniform exactly twice
-    # You may use at most 2 loops
-    for _ in range(iterations):
-        # Calculate the distance between each data point and each centroid
-        # You should use numpy.linalg.norm exactly twice
-        # You may use at most 2 loops
-
-        initial_c = np.copy(C)
-        c_ext = C[:, np.newaxis]
-        dist = np.sqrt(((X - c_ext) ** 2).sum(axis=2))
-
-        # Assign each data point to the nearest cluster centroid
-        clss = np.argmin(dist, axis=1)
-
-        # Update the centroid of each cluster
-        # If a cluster contains no data points during the update step,
-        # reinitialize its centroid
-        # You should use numpy.random.uniform exactly twice
-        # You may use at most 2 loops
+    n, d = X.shape
+    min = np.min(X, axis=0)
+    max = np.max(X, axis=0)
+    C = np.random.uniform(min, max, (k, d))
+    for i in range(iterations):
+        clss = np.argmin(np.linalg.norm(X[:, np.newaxis] - C, axis=-1), axis=1)
         for j in range(k):
-            if X[clss == j].size == 0:
-                C[j] = np.random.uniform(min, max, size=(1, d))
+            if np.sum(clss == j) == 0:
+                C[j] = np.random.uniform(min, max, (1, d))
             else:
                 C[j] = np.mean(X[clss == j], axis=0)
-
-        if (initial_c == C).all():
-            break
-
     return C, clss
