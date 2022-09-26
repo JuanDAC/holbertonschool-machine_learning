e683faabfe853fd839b6a463e3b2323ea19b50bf
JuanDAC <juanavila12.0308@gmail.com>
2022-09-25 23:14:11 -0500

    [🐛] (fix veriance generation un optimization)

--- unsupervised_learning/0x01-clustering/5-pdf.py
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -28,6 +28,12 @@ def pdf(X, m, S):
     if not isinstance(S, np.ndarray) or len(S.shape) != 2:
         return None
 
+    if X.shape[1] != m.shape[0] or X.shape[1] != S.shape[0]:
+        return None
+
+    if S.shape[0] != S.shape[1]:
+        return None
+
     n, d = X.shape
     _, d1 = m.shape
     d2, _ = S.shape
@@ -35,11 +41,18 @@ def pdf(X, m, S):
     if d != d1 or d != d2:
         return None
 
-    det = np.linalg.det(S)
-    inv = np.linalg.inv(S)
-    fac = 1 / np.sqrt(((2 * np.pi) ** d) * det)
-    exp = np.exp(-0.5 * np.einsum('...k,kl,...l->...', X - m, inv, X - m))
-    P = fac * exp
+    sigma_1 = np.linalg.det(S) ** (1/2)
+
+    sigma_2 = np.linalg.inv(S)
+    operation_1 = np.matmul((X - m), sigma_2)
+    operation_2 = np.sum((X - m) * operation_1, axis=1)
+
+    equation_1 = 1/(pi * sigma_1)
+    equation_2 = np.exp((-1/2) * operation_2)
+
+    PDF = equation_1 * equation_2
+    P = np.squeeze(PDF)
 
     P = np.where(P < 1e-300, 1e-300, P)
+
     return P
