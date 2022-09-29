754b88107b74597dbd07c6a3ee4719c986fdb752
JuanDAC <juanavila12.0308@gmail.com>
2022-09-29 13:14:42 -0500

    [🐛] (fix error of forward propagation)

--- supervised_learning/0x0D-RNNs/1-rnn.py
+++ supervised_learning/0x0D-RNNs/1-rnn.py
@@ -16,7 +16,8 @@ def rnn(rnn_cell, X, h_0):
             * t is the maximum number of time steps
             * m is the batch size
             * i is the dimensionality of the data
-        - h_0 is the initial hidden state, given as a numpy.ndarray of shape (m, h)
+        - h_0 is the initial hidden state, given as a numpy.ndarray of
+              shape (m, h)
             * h is the dimensionality of the hidden state
     Returns: H, Y
         - H is a numpy.ndarray containing all of the hidden states
