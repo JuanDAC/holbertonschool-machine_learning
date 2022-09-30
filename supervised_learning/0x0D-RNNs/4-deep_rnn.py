0746da1b16760ef7acb5bc0479735563b2665468
JuanDAC <juanavila12.0308@gmail.com>
2022-09-30 08:53:13 -0500

    [🚀] (add function of the propagation )

--- supervised_learning/0x0D-RNNs/4-deep_rnn.py
+++ supervised_learning/0x0D-RNNs/4-deep_rnn.py
@@ -6,6 +6,37 @@ File that contains the class LSTMCell
 import numpy as np
 
 
+def deep_rnn_cell(rnn_cells, x_t, h_prev):
+    """
+    Function that performs forward propagation for one time step for
+    a deep RNN
+    Arguments:
+        - rnn_cells is a list of RNNCell instances of length l that will
+                    be used for the forward propagation
+            * l is the number of layers
+        - x_t is the data input for the cell
+        - h_prev is a numpy.ndarray of shape (l, m, h) containing the
+                previous hidden states
+    Returns: h_next, y
+        - h_next is a numpy.ndarray of shape (l, m, h) containing the
+                next hidden states
+        - y is a numpy.ndarray of shape (l, m, o) containing the outputs
+    """
+    l, m, h = h_prev.shape
+    h_next = np.zeros((l, m, h))
+    y = np.zeros((l, m, rnn_cells[-1].by.shape[1]))
+    for layer in range(l):
+        if layer == 0:
+            h_next[layer], y[layer] = rnn_cells[layer].forward(
+                h_prev[layer],
+                x_t)
+        else:
+            h_next[layer], y[layer] = rnn_cells[layer].forward(
+                h_prev[layer],
+                h_next[layer - 1])
+    return h_next, y
+
+
 def deep_rnn(rnn_cells, X, h_0):
     """
     Function that performs forward propagation for a deep RNN
@@ -27,17 +58,10 @@ def deep_rnn(rnn_cells, X, h_0):
     """
     t, m, i = X.shape
     l, _, h = h_0.shape
-    H = np.zeros((t + 1, l, m, h))
-    H[0] = h_0
+    H = np.zeros((l, t + 1, m, h))
+    Y = np.zeros((l, t, m, rnn_cells[-1].by.shape[1]))
+    H[:, 0] = h_0
     for t in range(t):
-        for layer in range(l):
-            if layer == 0:
-                H[t + 1,
-                    layer], _ = rnn_cells[layer].forward(H[t, layer], X[t])
-            else:
-                H[t + 1, layer], _ = rnn_cells[layer].forward(
-                    H[t, layer], H[t + 1, layer - 1])
-    Y = np.zeros((t, m, rnn_cells[-1].by.shape[1]))
-    for t in range(t):
-        Y[t] = rnn_cells[-1].forward(H[t + 1, -1], H[t + 1, -2])[1]
+        H[:, t + 1, :, :], Y[:, t, :, :] = deep_rnn_cell(rnn_cells, X[t],
+                                                         H[:, t])
     return H, Y
