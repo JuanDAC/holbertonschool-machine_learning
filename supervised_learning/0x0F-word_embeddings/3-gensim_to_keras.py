0cba782d89693eb96ea3ec475d9737051af0544a
JuanDAC <juanavila12.0308@gmail.com>
2022-10-02 22:19:44 -0500

    [🌱] (add gensim to keras)

--- supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""
+File that contains the gensim_to_keras function
+"""
+
+
+def gensim_to_keras(model):
+    """
+    Function that converts a gensim word2vec model to a keras Embedding layer:
+    Arguments:
+        - model is a trained gensim word2vec models
+    Returns:
+        - the trainable keras Embedding
+    """
+    return model.wv.get_keras_embedding(train_embeddings=False)
