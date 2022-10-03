35da77138f408829201b9646e72a97756541eccf
JuanDAC <juanavila12.0308@gmail.com>
2022-10-03 00:36:24 -0500

    [🐛] (fix ngram bleu with for)

--- supervised_learning/0x10-nlp_metrics/1-ngram_bleu.py
+++ supervised_learning/0x10-nlp_metrics/1-ngram_bleu.py
@@ -5,21 +5,48 @@ File that contains the function ngram_bleu
 import numpy as np
 
 
-def transform_grams(references, sentence, size_n_gram):
+def ngram_bleu(references, sentence, n):
     """
     Function that calculates the n-gram BLEU score for a sentence:
     Arguments:
         - references is a list of reference translations
-            * each reference translation is a list of the words in the
-              translation
+            * each reference translation is a list of the words in the translation
         - sentence is a list containing the model proposed sentence
         - n is the size of the n-gram to use for evaluation
     Returns:
         - the n-gram BLEU score
     """
-    references = [ref[i:i+size_n_gram]
-                  for ref in references for i in range(len(ref))]
-    sentence = [sentence[i:i+size_n_gram] for i in range(len(sentence))]
-    references = list(map(tuple, references))
-    sentence = list(map(tuple, sentence))
-    return references, sentence
+    len_sentence = len(sentence)
+    list_references = []
+    for reference in references:
+        len_reference = len(reference)
+        list_references.append(
+            ((abs(len_reference - len_sentence)), len_reference))
+
+    reference_len = sorted(list_references, key=lambda x: x[0])
+
+    reference_len = reference_len[0][1]
+
+    count_dict = {}
+    for reference in references:
+        for i in range(len(sentence) - n + 1):
+            ngram = tuple(sentence[i:i + n])
+            if ngram not in count_dict.keys():
+                count_dict[ngram] = reference.count(ngram)
+                continue
+            new = reference.count(ngram)
+            old = count_dict[ngram]
+            count_dict[ngram] = max(new, old)
+
+    count_dict = count_dict.values()
+    count_dict = sum(count_dict)
+    precision = count_dict / len_sentence
+
+    if len_sentence > reference_len:
+        bp = 1
+    else:
+        bp = np.exp(1 - (reference_len / len_sentence))
+
+    bleu_score = bp * precision
+
+    return bleu_score
