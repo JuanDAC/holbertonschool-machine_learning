4ce8d75cbc065fee39eb9792eac20c8929c3b3dd
JuanDAC <juanavila12.0308@gmail.com>
2022-10-08 23:14:38 -0500

    [🐛] (fix reshape)

--- unsupervised_learning/0x03-hyperparameter_tuning/1-gp.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/1-gp.py
@@ -69,6 +69,6 @@ class GaussianProcess:
         K_s = self.kernel(self.X, X_s)
         K_ss = self.kernel(X_s, X_s)
         K_inv = np.linalg.inv(K)
-        mu = K_s.T.dot(K_inv).dot(self.Y)
+        mu = K_s.T.dot(K_inv).dot(self.Y).reshape(X_s.shape[0])
         sigma = np.diag(K_ss - K_s.T.dot(K_inv).dot(K_s))
         return mu, sigma
