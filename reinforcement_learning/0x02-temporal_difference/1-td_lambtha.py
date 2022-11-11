b8cf73d57b27d4465f8176635836a56ec981ef46
JuanDAC <juanavila12.0308@gmail.com>
2022-11-10 19:13:05 -0500

    (🚀) [add temporal diferents]

--- reinforcement_learning/0x02-temporal_difference/1-td_lambtha.py
+++ reinforcement_learning/0x02-temporal_difference/1-td_lambtha.py
@@ -21,7 +21,7 @@ def td_lambtha(env, V, policy, lambtha, episodes=5000, max_steps=100, alpha=0.1,
     for _ in range(episodes):
         state = env.reset()
         eligibility = np.zeros((V.shape))
-        for step in range(max_steps):
+        for _ in range(max_steps):
             action = policy(state)
             new_state, reward, done, info = env.step(action)
             delta = reward + gamma * V[new_state] - V[state]
